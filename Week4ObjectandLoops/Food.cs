de78830e3c9b08bd0fde800e515369985146fa9c
Jerome Smith <jerome.smith973@gmail.com>
2022-09-20 21:56:15 -0400

    Add Week4ObjectandLoops

---
+++ Week4ObjectandLoops/Food.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week4ObjectandLoops
+{
+    internal class Food
+    {
+        int fID;
+        double cost;
+        string name, description;
+
+        public int FoodID
+
+
+        { 
+            get { return fID; } 
+            set { fID = value; }
+        } 
+
+        public double Cost
+
+        { 
+            get { return cost; }
+            set { cost = value; } 
+        }
+        
+        public string Name
+
+        { get { return name; } 
+          set { name = value; }
+
+        
+        
+        }
+                
+
+        public string Description
+
+        {    
+            get { return description; }
+            set { description = value; } 
+        
+        }
+
+            
+            
+
+    }
+    
+
+     
+}
---
+++ Week4ObjectandLoops/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Week4ObjectandLoops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Food fd1 = new Food();
+            Food fd2 = new Food();
+
+            fd1.FoodID = 1;
+            fd1.Name = "Pizza";
+            fd1.Cost = 15.00;
+            fd1.Description = "A no topping cheese pizza.";
+
+            fd2.FoodID = 2;
+            fd2.Name = "Cheeseburger";
+            fd2.Cost = 10.00;
+            fd2.Description = "An ordinary Cheeseburger.";
+
+
+            WriteLine("{0,10}{1,10}{2,10}{3,10}", "Food #", "Name", "Cost", "Description");
+            WriteLine("{0,10}{1,10}{2,10}{3,10}", fd1.FoodID, fd1.Name, fd1.Cost, fd1.Description);
+            WriteLine("{0,10}{1,10}{2,10}{3,10}", fd2.FoodID, fd2.Name, fd2.Cost, fd2.Description);
+
+
+        }
+    }
+}
