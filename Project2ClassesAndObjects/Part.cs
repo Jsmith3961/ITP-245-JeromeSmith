2bbcf2eefb20d39df42f9357e0cdd982fa27f270
Jerome Smith <jerome.smith973@gmail.com>
2022-12-02 21:05:36 -0500

    Add Project2

---
+++ Project2ClassesAndObjects/Part.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Project2ClassesAndObjects
+{
+    internal class Part
+    {
+
+        int partNum, quantity;
+        double cost, value;
+        string name, description;
+
+        public int PartNumber
+        {
+            get; 
+            set;
+        }
+        
+        public double Cost
+        {
+            get;
+            set;
+        }
+
+        public int Quantity
+        {
+            get;
+            set;
+        }
+
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        public string Description
+        {
+            get;
+            set;
+        }
+
+        public double Value
+        {
+            get { return Cost * quantity; }
+        }
+
+        public Part() { }
+        public Part(int partNum, int quantity, double cost, string name, string description, int partNumber)
+
+        { 
+            Description = description;
+            PartNumber = partNumber;
+            Cost = cost;
+            Quantity = quantity;
+            Name = name; 
+        }
+        public string Display()
+        {
+            return $"\nNumber:\t\t{partNum}" +
+                $"\nName:\t\t{name}" +
+                $"\nDescription:\t{description}" +
+                $"\nQuantity:\t{Quantity}" +
+                $"\nCost:\t\t{Cost:c}" +
+                $"\nValue:\t\t{Value:c}";
+
+
+
+        }
+        }
+    }
---
+++ Project2ClassesAndObjects/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+namespace Project2ClassesAndObjects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Ronald's Part's, How many parts will you add?");
+            var parts = new List<Part>();
+            var partCount = Convert.ToInt16(Console.ReadLine());
+            var count = 0; Part part = null;
+            while (count < partCount)
+            {
+                count++;
+                Console.WriteLine($"\nEnter the Part Number for Part Number {count}");
+                var partNumber = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine($"\nEnter the Name for Part Number {partNumber}");
+                var name = Console.ReadLine();
+                Console.WriteLine($"\nEnter the Description for Part Number {partNumber}");
+                var description = Console.ReadLine();
+                Console.WriteLine($"\nEnter the Quantity for Part Number {partNumber}");
+                var quantity = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine($"\nEnter the cost for Part Number {partNumber}");
+                var cost = Convert.ToDouble(Console.ReadLine());
+                part = new Part (partNumber, name, description, cost, quantity);
+                parts.Add(part);
+
+            }
+            Part[] partArray = parts.ToArray();
+            Console.WriteLine("\n\nWhich part do you want to view? Enter the part number.");
+            for (int i = 0; i < partArray.Length; i++)
+                Console.WriteLine($"{partArray[i].PartNumber}\t{partArray[i].Name}");
+                var partNum = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < partNum; i++)
+            {
+                if (partArray[i].PartNumber == partNum)
+                {
+
+                    part = partArray[i];
+                    break;
+                }
+                Console.WriteLine($"{part.Display()}");
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
