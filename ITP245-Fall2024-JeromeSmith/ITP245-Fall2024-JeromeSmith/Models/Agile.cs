e8b2cb6b633eac89e3c1ac7f8b1c1b046f187de2
Bawz552 <jerome.smith973@gmail.com>
2024-09-16 20:21:42 -0400

    MVC Assignment

---
+++ ITP245-Fall2024-JeromeSmith/ITP245-Fall2024-JeromeSmith/Models/Agile.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ITP245_Fall2024_JeromeSmith.Models
+{
+    public class Contact
+    {
+
+        public string Author { get; set; }
+
+        public string Email  { get; set; }
+
+        public string Phone  { get; set;}
+
+        public string Description { get; set; }
+
+        public string Manage { get; set;}
+
+        public List<string> Manages { get; private set; }
+        public Contact()
+        {
+            Author = "Jerome Smith";
+            Email = "jhs39612@email.vccs.edu";
+            Phone = "804-386-7818";
+            Description = "Reynolds Community College software management for ITP Courses.";
+            Manage = "This manages both team and individual projects.";
+            Manages = new List<string>() { "Tasks", "Deadlines", "Progress and", "Other issues" };
+        }
+
+    }
+}
---
+++ ITP245-Fall2024-JeromeSmith/ITP245-Fall2024-JeromeSmith/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ITP245_Fall2024_JeromeSmith")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ITP245_Fall2024_JeromeSmith")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a95ccfb6-0811-482e-a85b-c6924789f369")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
