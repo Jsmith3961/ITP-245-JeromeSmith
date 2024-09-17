01fe54d30ac6aa60fa4a85e35ae58e7bbc7e958e
Bawz552 <jerome.smith973@gmail.com>
2024-09-17 02:40:18 -0400

    Ran into an error saying that im missing NuGet packages, unsure how to resolve this issue (ran into error comitting EDMX too, might require a reupload)

--- ITP245-Fall2024-JeromeSmith/ITP245-Fall2024-JeromeSmith/Models/Sports.cs
+++ ITP245-Fall2024-JeromeSmith/ITP245-Fall2024-JeromeSmith/Models/Sports.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using ITP245_Sports_Model;
 
 namespace ITP245_Fall2024_JeromeSmith.Models
 {
@@ -19,9 +20,14 @@ namespace ITP245_Fall2024_JeromeSmith.Models
         public string Education { get; set; }
 
         public List<string> Experience { get; private set; }
+
         public About()
         {
-            Name = "Jerome Smith";
+            using (var entities = new SportsEntities())
+              {
+                var systemOption = entities.SystemOptions.First();
+                Name = systemOption.Name; 
+               }
             Email = "jhs39612@email.vccs.edu";
             Hometown = "Paterson, New Jersey";
             Major = "Computer Science";
