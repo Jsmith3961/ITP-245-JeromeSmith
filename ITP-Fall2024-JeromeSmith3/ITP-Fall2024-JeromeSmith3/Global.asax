c5cbc0e5374c0c17fd51d3449dbd6e65083b207a
Jsmith3961 <jerome.smith973@gmail.com>
2024-11-22 19:25:26 -0500

    Week 3: Agile section errors.

---
+++ ITP-Fall2024-JeromeSmith3/ITP-Fall2024-JeromeSmith3/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ITP_Fall2024_JeromeSmith3.MvcApplication" Language="C#" %>
---
+++ ITP-Fall2024-JeromeSmith3/ITP-Fall2024-JeromeSmith3/Models/Agile.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ITP_Fall2024_JeromeSmith3;
+using ITP245_Agile_Model;
+
+namespace ITP_Fall2024_JeromeSmith3.Models
+{
+    public class Contact
+    {
+
+        public string Author { get; set; }
+
+        public string Email { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Description { get; set; }
+
+        public string Manage { get; set; }
+
+        public List<string> Manages { get; private set; }
+        public Contact()
+        {
+            using (var entities = new AgileEntities())
+
+            {
+                var systemOption = entities.SystemOptions.First();
+                Author = systemOption.Name;
+            }
+            Email = "jhs39612@email.vccs.edu";
+            Phone = "804-386-7818";
+            Description = "Reynolds Community College software management for ITP Courses.";
+            Manage = "This manages both team and individual projects.";
+            Manages = new List<string>() { "Tasks", "Deadlines", "Progress and", "Other issues" };
+        }
+    }
+}
